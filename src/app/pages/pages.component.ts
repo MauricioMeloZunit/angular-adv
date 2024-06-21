53f775280c1a61f752c264c424fe6e02a4d68b59
MauricioMeloZunit <148736167+MauricioMeloZunit@users.noreply.github.com>
2024-06-20 23:04:47 -0600

    Initial commit

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../shared/header/header.component';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from '../shared/breadcrumbs/breadcrumbs.component';
+
+@Component({
+  selector: 'app-pages',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent,BreadcrumbsComponent],
+  templateUrl: './pages.component.html',
+  styles: ``
+})
+export class PagesComponent {
+
+}
